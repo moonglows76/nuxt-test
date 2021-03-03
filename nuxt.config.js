1fe697cf4c1a0168ae620343e3076b9b862bd33f
森史憲 <mori@codingdesing.jp>
2021-03-03 12:15:51 +0900

    Googleフォントの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,17 @@ export default {
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-      { rel: 'manifest', href: '/site.webmanifest' }
+      { rel: 'manifest', href: '/site.webmanifest' },
+      {
+        rel: 'preconnect',
+        href:
+          'https://fonts.gstatic.com',
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet',
+      },
     ]
   },
 
